406abf7ae0163fe2b7dc3c3a6d59c7a5c69bea06
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2023-01-22 10:06:54 +0530

    Initial Commit

---
+++ Kwicpic Assesment/Kwicpic_AssesmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kwicpic_AssesmentApp.swift
+//  Kwicpic Assesment
+//
+//  Created by Adarsh Shukla on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Kwicpic_AssesmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
